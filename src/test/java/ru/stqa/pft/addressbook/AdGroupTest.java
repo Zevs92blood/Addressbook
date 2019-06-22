4ffe615592db8a9ee46da7f82d87fc6bed0d1d8e
Zevs <Zevs92blood@gmail.com>
2019-06-22 14:15:50 +0400

    ЗАРАБОТАЛО!!!

--- src/test/java/ru/stqa/pft/addressbook/AdGroupTest.java
+++ src/test/java/ru/stqa/pft/addressbook/AdGroupTest.java
@@ -3,7 +3,7 @@ package ru.stqa.pft.addressbook;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -11,12 +11,12 @@ import org.testng.annotations.Test;
 import java.util.concurrent.TimeUnit;
 
 public class AdGroupTest {
-  FirefoxDriver driver;
+  ChromeDriver driver;
 
 
   @BeforeMethod
   public void setUp() throws Exception {
-    driver = new FirefoxDriver();
+    driver = new ChromeDriver();
     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
   }
 
@@ -50,7 +50,7 @@ public class AdGroupTest {
     driver.quit();
   }
 
-  public static boolean isAlertPresent(FirefoxDriver driver){
+  public static boolean isAlertPresent(ChromeDriver driver){
     try{
       driver.switchTo().alert();
       return true;
