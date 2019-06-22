fa2a17631ae464bb7b94c33954971803526da193
Zevs <Zevs92blood@gmail.com>
2019-06-22 14:36:08 +0400

    Открытие браузера и авторизация перенесены в setUp

--- src/test/java/ru/stqa/pft/addressbook/AdGroupTest.java
+++ src/test/java/ru/stqa/pft/addressbook/AdGroupTest.java
@@ -18,10 +18,6 @@ public class AdGroupTest {
   public void setUp() throws Exception {
     driver = new ChromeDriver();
     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-  }
-
-  @Test
-  public void adGroup() {
     driver.get("http://192.168.56.101/addressbook/index.php");
     driver.findElement(By.name("user")).click();
     driver.findElement(By.name("user")).clear();
@@ -30,6 +26,11 @@ public class AdGroupTest {
     driver.findElement(By.name("pass")).clear();
     driver.findElement(By.name("pass")).sendKeys("secret");
     driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]")).click();
+  }
+
+  @Test
+  public void adGroup() {
+
     driver.findElement(By.linkText("groups")).click();
     driver.findElement(By.name("new")).click();
     driver.findElement(By.name("group_name")).click();
